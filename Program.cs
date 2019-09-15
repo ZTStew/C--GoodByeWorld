9de65a047c2ff7c92d683295282ea9cf65c31dad
[Zach.S] <[ZT.Stewart.5@gmail.com]>
2019-09-15 12:31:48 -0400

    First C#/.NET program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * First C#/.NET program writen
+ *
+ * Author: Stewart
+ * 9/15/2019
+ */
+
+using System;
+
+namespace GoodByeWorld {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("GoodBye World");
+        }
+    }
+}
